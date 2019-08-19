b70ba9e58c2cad3463c8e8d2985f49ae1dadf9ce
jkohm <35876819+jockeoh@users.noreply.github.com>
2019-08-19 20:56:11 +0200

    fun with redux

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import { Provider } from "react-redux";
 import App from "./app/layout/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
+import { configureStore } from "./app/store/configureStore";
+
+const store = configureStore();
+
+console.log(store.getState());
 
 const rootEl = document.getElementById("root");
 
 let render = () => {
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
     rootEl
   );
 };
@@ -19,7 +27,7 @@ let render = () => {
 if (module.hot) {
   module.hot.accept("./app/layout/App", () => {
     setTimeout(render);
-  });
+  }); // Makes changes instant in browser
 }
 
 render();
