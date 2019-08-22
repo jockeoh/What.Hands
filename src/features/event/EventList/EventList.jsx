dc53d301e0276c9dfb5aee15f70cc159a9dfcd5a
jkohm <35876819+jockeoh@users.noreply.github.com>
2019-08-22 21:36:19 +0200

    refactor redux

--- src/features/event/EventList/EventList.jsx
+++ src/features/event/EventList/EventList.jsx
@@ -3,16 +3,11 @@ import EventListItem from "./EventListItem";
 
 class EventList extends Component {
   render() {
-    const { events, selectEvent, deleteEvent } = this.props;
+    const { events, deleteEvent } = this.props;
     return (
       <Fragment>
         {events.map(event => (
-          <EventListItem
-            key={event.id}
-            event={event}
-            deleteEvent={deleteEvent}
-            selectEvent={selectEvent}
-          />
+          <EventListItem key={event.id} event={event} deleteEvent={deleteEvent} />
         ))}
       </Fragment>
     );
